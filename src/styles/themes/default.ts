c5a0ebcb8d80e5ecb230cfb74bf2951057a46718
mathiasfuhr <mathiasgilvanf@gmail.com>
2024-08-14 04:32:09 -0300

    first commit

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,37 @@
+export const defaultTheme = {
+    white: "#ffffff",
+    
+    "gray-100": "#e1e1e6",
+    "gray-300": "#c4c4cc",
+    "gray-400": "#8d8d99",
+    "gray-500": "#7c7c8a",
+    "gray-600": "#323238",
+    "gray-700": "#29292e",
+    "gray-800": "#202024",
+    "gray-900": "#121214",
+  
+    "green-100": "#ccffcc",
+    "green-300": "#00b37e",
+    "green-500": "#00875f",
+    "green-700": "#015f43",
+    "green-900": "#003820",
+  
+    "blue-100": "#cce4ff",
+    "blue-300": "#66b2ff",
+    "blue-500": "#3399ff",
+    "blue-700": "#0066cc",
+    "blue-900": "#003366",
+  
+    "red-100": "#ffcdd2",
+    "red-300": "#e57373",
+    "red-500": "#f44336",
+    "red-700": "#d32f2f",
+    "red-900": "#b71c1c",
+  
+    "yellow-100": "#fff9c4",
+    "yellow-300": "#fff176",
+    "yellow-500": "#ffeb3b",
+    "yellow-700": "#fbc02d",
+    "yellow-900": "#f57f17",
+  } as const;
+  
