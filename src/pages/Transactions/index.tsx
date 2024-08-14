aea337873180025e6a87429f55773b2d9680651f
mathiasfuhr <mathiasgilvanf@gmail.com>
2024-08-14 05:18:15 -0300

    feat: add Transactions component with styling

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import Header from "../../components/Header"
 import Summary from "../../components/Summary"
+import { PriceHightLight, TransactionsContainer, TransactionsTable } from "./styles"
 
 const Transactions = () => {
   return (
     <div>
-        <Header/>
-        <Summary/>
+      <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width='50%'>Desenvolimento de site</td>
+              <td>
+                <PriceHightLight variant="income">
+                  R$ 12.000,00
+                </PriceHightLight>
+              </td>
+              <td>venda</td>
+              <td>14/08/2024</td>
+            </tr>
+            <tr>
+              <td width='50%'>Hamburger</td>
+              <td>
+                <PriceHightLight variant="outcome">
+                  - R$ 59,00
+                </PriceHightLight>
+              </td>
+              <td>Alimentação</td>
+              <td>14/08/2024</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
