dbfead538722c1a852f5383ab20c7bb56ce647e2
mathiasfuhr <mathiasgilvanf@gmail.com>
2024-08-14 08:42:16 -0300

    chore: create hooks folder and separate hooks into a dedicated file

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,28 +1,10 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react"
 import { SummuryCard, SummuryContainer } from "./styles"
-import { useContext } from "react"
-import { TransactionsContext } from "../../contexts/TransactionsContext"
 import { priceformatter } from "../../utils/formatter"
+import { useSummary } from "../../hooks/UseSummary"
 
 const Summary = () => {
-    const { transactions } = useContext(TransactionsContext)
-
-    const summary = transactions.reduce(
-        (acc, transaction) => {
-            if (transaction.type === 'income') {
-                acc.income += transaction.price;
-                acc.total += transaction.price;
-            } else {
-                acc.outcome += transaction.price
-                acc.total += transaction.price;
-            }
-            return acc
-        },
-        {
-            income: 0,
-            outcome: 0,
-            total: 0
-        })
+    const summary = useSummary()
 
     return (
         <SummuryContainer>
